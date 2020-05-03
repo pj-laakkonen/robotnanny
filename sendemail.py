fb301fcc6ff1d95c25a35dc1a04cb24dfb9655ea
pj-laakkonen <petri@laakkonen.io>
2020-05-03 17:18:10 +0300

    sendemail text

--- sendemail.py
+++ sendemail.py
@@ -6,6 +6,10 @@ def send_email():
 
     contents = [
         "Okay Albert, it's time to go sleep now :)",
-        #" ",
+        "---------------------------------------------------------------",
+        "Mobile data and wifi will be blocked after 15 minutes at 23.",
+        "Blocks will be removed in the morning at 7.",
+        "---------------------------------------------------------------",
+        "Good night",
     ]
     yag.send('recipient@gmail.com', 'Bedtime', contents)
